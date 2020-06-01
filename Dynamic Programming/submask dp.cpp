b0ceddc47c98bf0bf6089a636a8fd507cf2fe8a4
thesafatem <thesafatem@gmail.com>
2020-06-01 23:38:38 +0600

    submasks

---
+++ Dynamic Programming/submask dp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+	int n; cin >> n;
+	int cnt = 0;
+	int cube = 1;
+	for (int i = 1; i <= n; i++) {
+		cube *= 3;
+	}
+	for (int mask = 0; mask < (1 << n); mask++) {
+		for (int submask = mask; submask > 0; submask = (submask - 1) & mask) {
+			// operations with mask and submask
+			cnt++;
+		}
+	}
+	cout << cnt << " " << cube << endl;
+}
+
+int main() {
+	solve();
+}
